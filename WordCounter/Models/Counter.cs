844a2ec8d6f3aa24b4b2d0eb6778ff8b6526b0a0
Student <example@example.com>
2019-07-26 10:50:36 -0700

    ask user for input and ouput frequency

--- WordCounter/Models/Counter.cs
+++ WordCounter/Models/Counter.cs
@@ -6,8 +6,53 @@ namespace WordCounter.Models
 
     public static class Counter
     {
-        public static int WordFrequency(string sentence){
-            return 3;
+        public static int WordFrequency(string input, string sentence){
+            
+            char[] ignoreChars = new char[]{'\''};
+
+            int count = 0;
+            string currentStr = "";
+            for(int i = 0; i < sentence.Length; i++){
+                //special case
+                if (sentence[i] != '\''){
+                     currentStr += sentence[i];
+                
+                if (currentStr == input){
+                    if (i < sentence.Length - 1){
+                        if (isResetChar(sentence[i+1])){
+                             count++;
+                             currentStr = "";
+                        }
+                    } else { //last character of sentence
+                        count++;
+                        currentStr = "";
+                    }
+                } else {
+                    if (isResetChar(sentence[i])){
+                        currentStr = "";
+                    }
+                }
+
+                
+                }
+                //case where ' is last char
+                if (i == sentence.Length - 1 && sentence[i] == '\''){
+                    if (currentStr == input){
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    //need to check spaces
+        public static bool isResetChar(char c){
+            char[] resetChars = new char[]{' ', ',','.','?','!','.',';','(',')','&','*'};
+            for(int i = 0; i < resetChars.Length; i++){
+                if (resetChars[i] == c){
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
--- WordCounter/Models/Program.cs
+++ WordCounter/Models/Program.cs
@@ -1,11 +1,39 @@
 using System;
 using System.Collections.Generic;
 
+
 namespace WordCounter.Models{
 public class Program{
 
-    public static void Main(){
-        Console.WriteLine("ER");
+        public static void Main(){
+            do{
+            Console.WriteLine("This program takes in a word and a sentence and outputs how many times the word appeared in the sentence.");
+            Console.WriteLine("Enter the sentence:");
+            string sentence = Console.ReadLine();
+            while(sentence == ""){
+                Console.WriteLine("You just pressed the enter key instead of typing in the sentence and then pressing the enter key. Type the sentence first, then press enter. Two steps. You can do this.");
+                sentence = Console.ReadLine();
+            }
+            Console.WriteLine("Good. This is great so far.");
+            Console.WriteLine("Go ahead and enter the word and we will go from there: ");
+            string word = Console.ReadLine();
+            while(word == ""){
+                Console.WriteLine("You just pressed enter. You have to type in the word, then press enter! ffs!");
+                word = Console.ReadLine();
+            }
+            Console.WriteLine(word + " appeared " + Counter.WordFrequency(word, sentence) + " in the sentence you inputted. Nicely done. That is better than 37% of users!");
+        
+        } while(Program.PlayAgain());
+        Console.WriteLine("Until next time, Dave");
+        }
+
+        public static bool PlayAgain(){
+            Console.WriteLine("Would you like to enter another sentence?");
+            string input = Console.ReadLine().ToLower();
+            if (input == "n" || input == "no" || input == "nope" || input == "noo"){
+                return false;
+            }
+            return true;
+        }
     }
-}
 }
