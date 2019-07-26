bcd72110fa719617f51184bb69b4f06aad7e8641
Student <example@example.com>
2019-07-26 10:26:30 -0700

    calculate word frequency

--- WordCounter.Tests/WordCounter.Tests.cs
+++ WordCounter.Tests/WordCounter.Tests.cs
@@ -10,11 +10,27 @@ namespace WordCounter.Tests{
     public class FrequencyTest
     {
         [TestMethod]
-        public void Frequency_NumberofAppearances_int()
+        public void Frequency_NumberofAppearances_3()
         {   
             
-            int result = Counter.WordFrequency("hello hello");
+            int result = Counter.WordFrequency("hi","hi hi.hi.");
             Assert.AreEqual(result, 3);
+        }
+
+        [TestMethod]
+        public void Frequency_NumberofAppearances_4()
+        {   
+            
+            int result = Counter.WordFrequency("hi","a.hi.f.hit.hi.hi.hi.");
+            Assert.AreEqual(result, 4);
+        }
+
+        [TestMethod]
+        public void Frequency_NumberofAppearances_5()
+        {   
+            
+            int result = Counter.WordFrequency("hi","h'i hi hi hi hi adfhi hi'");
+            Assert.AreEqual(result, 5);
         }
     }
 }
